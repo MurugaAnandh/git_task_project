b74613aab7341c31b1cafb44cea1ed204a3f9351
MurugaAnandh <murugaanandh03@gmail.com>
2024-03-10 16:11:24 +0000

    modified_file v3.0

--- helloWorld.py
+++ helloWorld.py
@@ -3,5 +3,9 @@
 # Used print statement to print "Hello World!"
 
 #print(Hello World!)
+
 user_name = input("Enter your name: ")
+
+#Added Welcome Message
+print(f"Welcome to git, {user_name}")
 print(f"Git is Awesome")
